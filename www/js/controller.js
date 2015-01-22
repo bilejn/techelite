17f1e7ac3a04f28dd6ca8b20bd5c6ba9df955d5a
Bilejn <bilejn_nik@yahoo.com>
2015-01-22 23:41:46 +0100

    checkConnection()

--- www/js/controller.js
+++ www/js/controller.js
@@ -1,28 +1,6 @@
 $(document).ready(function(){
 	
-	  try{
-        var networkState = navigator.connection.type;
-
-        setTimeout(function(){
-            networkState = navigator.connection.type;
-
-            var states = {};
-            states[Connection.UNKNOWN]  = 'Unknown connection';
-            states[Connection.ETHERNET] = 'Ethernet connection';
-            states[Connection.WIFI]     = 'WiFi connection';
-            states[Connection.CELL_2G]  = 'Cell 2G connection';
-            states[Connection.CELL_3G]  = 'Cell 3G connection';
-            states[Connection.CELL_4G]  = 'Cell 4G connection';
-            states[Connection.NONE]     = 'No network connection';
-
-            alert('Connection type: ' + states[networkState]);
-        }, 2000);
-    }catch(e){
-        alert(e);
-        $.each(navigator, function(key, value){
-            alert(key+' => '+value);
-        });
-    }
+	  checkConnection();
 
 });
 
--- www/js/network.js
+++ www/js/network.js
@@ -1,16 +1,29 @@
 function checkConnection() {
-    var networkState = navigator.connection.type;
 
-    var states = {};
-    states[Connection.UNKNOWN]  = 'Unknown connection';
-    states[Connection.ETHERNET] = 'Ethernet connection';
-    states[Connection.WIFI]     = 'WiFi connection';
-    states[Connection.CELL_2G]  = 'Cell 2G connection';
-    states[Connection.CELL_3G]  = 'Cell 3G connection';
-    states[Connection.CELL_4G]  = 'Cell 4G connection';
-    states[Connection.CELL]     = 'Cell generic connection';
-    states[Connection.NONE]     = 'No network connection';
+   try{
+        var networkState = navigator.connection.type;
 
-    alert( states[networkState]);
+        setTimeout(function(){
+            networkState = navigator.connection.type;
+
+            var states = {};
+            states[Connection.UNKNOWN]  = 'Unknown connection';
+            states[Connection.ETHERNET] = 'Ethernet connection';
+            states[Connection.WIFI]     = 'WiFi connection';
+            states[Connection.CELL_2G]  = 'Cell 2G connection';
+            states[Connection.CELL_3G]  = 'Cell 3G connection';
+            states[Connection.CELL_4G]  = 'Cell 4G connection';
+            states[Connection.NONE]     = 'No network connection';
+		if (states[networkState] == 'No network connection'){
+			alert("No network connection. Please connect to internet.");
+		}
+            
+        }, 2000);
+    }catch(e){
+        alert(e);
+        $.each(navigator, function(key, value){
+            alert(key+' => '+value);
+        });
+    }
 }
 
