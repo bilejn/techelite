ddae577bbc3be8528b7d84d08f45e70ea0fc551e
Bilejn <bilejn_nik@yahoo.com>
2015-01-22 22:48:24 +0100

    network state plugin4

--- www/js/controller.js
+++ www/js/controller.js
@@ -1,7 +1,27 @@
-$(document).ready(function(){
-	
-	checkConnection();
+function onDeviceReady(){
+    try{
+        var networkState = navigator.connection && navigator.connection.type;
 
-});
+        setTimeout(function(){
+            networkState = navigator.connection && navigator.connection.type;
+
+            var states = {};
+            states[Connection.UNKNOWN]  = 'Unknown connection';
+            states[Connection.ETHERNET] = 'Ethernet connection';
+            states[Connection.WIFI]     = 'WiFi connection';
+            states[Connection.CELL_2G]  = 'Cell 2G connection';
+            states[Connection.CELL_3G]  = 'Cell 3G connection';
+            states[Connection.CELL_4G]  = 'Cell 4G connection';
+            states[Connection.NONE]     = 'No network connection';
+
+            alert('Connection type: ' + states[networkState]);
+        }, 500);
+    }catch(e){
+        alert(e);
+        $.each(navigator, function(key, value){
+            alert(key+' => '+value);
+        });
+    }
+}
 
 					
