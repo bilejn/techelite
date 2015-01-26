044720c0fc9a633d6f7624e0484c6b5e44732429
Bilejn <bilejn_nik@yahoo.com>
2015-01-26 09:52:09 +0100

    deviceReady 3

--- www/js/network.js
+++ www/js/network.js
@@ -2,7 +2,6 @@ function checkConnection() {
 
    try{
         var networkState = navigator.connection.type;
-		alert("checking connection.");
         setTimeout(function(){
             networkState = navigator.connection.type;
 
