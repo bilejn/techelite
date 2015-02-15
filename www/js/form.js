c33f6379ef9b12c8e6ca6746ee8b94eff16f2f97
Bilejn <bilejn_nik@yahoo.com>
2015-02-15 21:27:44 +0100

    loadar dyn

--- www/js/form.js
+++ www/js/form.js
@@ -187,6 +187,7 @@ var form = (function() {
 	
 		/*Method deleteJson deletes "json_data" and reloads the window. The function is called from clear button (home section - end) . */		
 		deleteJson: function () {
+			$.jStorage.flush();
 			var r = confirm(locals[cLANGUAGE].confirmDelete);
 			if (r == true) {
 				 $.jStorage.deleteKey("json_data"); 
