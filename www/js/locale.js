217da1d1d7e8ff5c7dc475d669946e66f3917d53
Bilejn <bilejn_nik@yahoo.com>
2015-02-15 16:53:11 +0100

    loaden() 3

--- www/js/locale.js
+++ www/js/locale.js
@@ -67,22 +67,14 @@
 	  filerefjs.setAttribute("src", "js/rtl.jquery.mobile-1.4.0.js");
 		document.getElementsByTagName("head")[0].appendChild(filerefjs);		
 		
-		removejscssfile("jquery.mobile-1.4.2.js", "js") //remove all occurences of "somescript.js" on page
-		removejscssfile("jquery.mobile-1.4.2.css", "css") //remove all occurences "somestyle.css" on page	
+		document.getElementsByTagName("head")[0].removeChild(document.getElementById("css"));
+		document.getElementsByTagName("head")[0].removeChild(document.getElementById("js"));
+		
 		
 		$("#home").trigger("pagebeforeshow");
 
 	}
 	
-	function removejscssfile(filename, filetype){
-	 var targetelement=(filetype=="js")? "script" : (filetype=="css")? "link" : "none" //determine element type to create nodelist from
-	 var targetattr=(filetype=="js")? "src" : (filetype=="css")? "href" : "none" //determine corresponding attribute to test for
-	 var allsuspects=document.getElementsByTagName(targetelement)
-	 for (var i=allsuspects.length; i>=0; i--){ //search backwards within nodelist for matching elements to remove
-	  if (allsuspects[i] && allsuspects[i].getAttribute(targetattr)!=null && allsuspects[i].getAttribute(targetattr).indexOf(filename)!=-1)
-	   allsuspects[i].parentNode.removeChild(allsuspects[i]) //remove element by calling parentNode.removeChild()
-	 }
-	}
 
 
 	
